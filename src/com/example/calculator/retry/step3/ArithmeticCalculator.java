57dca96a71b88e66960cb0cb82d61b49bf681f50
최정윤 <jes2ngyun@gmail.com>
2026-06-22 17:35:49 +0900

    calculate 메서드 구조 구현

--- src/com/example/calculator/retry/step3/ArithmeticCalculator.java
+++ src/com/example/calculator/retry/step3/ArithmeticCalculator.java
@@ -2,4 +2,12 @@ package com.example.calculator.retry.step3;
 
 public class ArithmeticCalculator<T extends Number> {
 
+    public double calculate(T num1, T num2, OperatorType operator) {
+
+        double first = num1.doubleValue();
+        double second = num2.doubleValue();
+
+        double result;
+    }
+
 }
