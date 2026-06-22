8641c4ce3bbc2b99a8b03e8e0b4e9b50855193ec
최정윤 <jes2ngyun@gmail.com>
2026-06-22 17:14:32 +0900

    연산자 변환 메서드 추가

--- src/com/example/calculator/retry/step3/OperatorType.java
+++ src/com/example/calculator/retry/step3/OperatorType.java
@@ -6,4 +6,24 @@ public enum OperatorType {
     SUBTRACT('-'),
     MULTIPLY('*'),
     DIVIDE('/');
+
+    private final char symbol;
+
+    OperatorType(char symbol) {
+        this.symbol = symbol;
+    }
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public static OperatorType fromSymbol(char symbol) {
+
+        for (OperatorType operator : values()) {
+
+            if ( operator.symbol == symbol) {
+                return operator;
+            }
+        }
+        throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
+    }
 }
