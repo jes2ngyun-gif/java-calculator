f2adbd1e1a1c88f08f5792709be3276817e45e2e
최정윤 <jes2ngyun@gmail.com>
2026-06-22 18:02:36 +0900

    입출력 반복 기능 구현

--- src/com/example/calculator/retry/step3/CalculatorApp.java
+++ src/com/example/calculator/retry/step3/CalculatorApp.java
@@ -1,8 +1,14 @@
 package com.example.calculator.retry.step3;
 
+import java.util.Scanner;
+
 public class CalculatorApp {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArithmeticCalculator<Double> calculator = new ArithmeticCalculator<>();
 
     }
 }
