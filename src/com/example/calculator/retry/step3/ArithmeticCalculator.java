136c04dbf54aec50a1b6167f54349572adceba5a
최정윤 <jes2ngyun@gmail.com>
2026-06-22 17:44:27 +0900

    enum 기반 사칙연산 구현

--- src/com/example/calculator/retry/step3/ArithmeticCalculator.java
+++ src/com/example/calculator/retry/step3/ArithmeticCalculator.java
@@ -2,12 +2,38 @@ package com.example.calculator.retry.step3;
 
 public class ArithmeticCalculator<T extends Number> {
 
+
     public double calculate(T num1, T num2, OperatorType operator) {
 
         double first = num1.doubleValue();
         double second = num2.doubleValue();
 
         double result;
+
+        switch (operator) {
+
+            case ADD:
+                result = first + second;
+                break;
+
+            case SUBTRACT:
+                result = first - second;
+                break;
+
+            case MULTIPLY:
+                result = first * second;
+
+            case DIVIDE:
+                if (second == 0) {
+                    throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+                }
+                result = first / second;
+                break;
+
+            default:
+                throw new IllegalArgumentException("잘못된 연산입니다.");
+        }
+
     }
 
 }
